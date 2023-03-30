15cd9df262d8e09a69cd080c9c019e4464316c96
yas-say <yasowant@live.com>
2023-03-29 22:37:58 -0400

    email

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import smtplib
+import datetime as dt
+import pandas
+import random
 
-with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-    connection.starttls()
-    connection.login(user="yasowant@gmail.com", password="mngejbzmcoejdmhs")
-    connection.sendmail(from_addr="yasowant@gmail.com", to_addrs="sarita.dash28@gmail.com", msg="Subject:Hello \n\nYou been hacked.")
+# with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+#     connection.starttls()
+#     connection.login(user="yasowant@gmail.com", password="mngejbzmcoejdmhs")
+#     connection.sendmail(from_addr="yasowant@gmail.com", to_addrs="sarita.dash28@gmail.com", msg="Subject:Hello \n\nYou been hacked.")
+
+now = dt.datetime.now()
+
+
+with open("quotes.txt") as file:
+    quotes = file.readlines()
+
+
+if now.isoweekday() == 3:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+         connection.starttls()
+         connection.login(user="yasowant@gmail.com", password="mngejbzmcoejdmhs")
+         connection.sendmail(from_addr="yasowant@gmail.com", to_addrs="yasowant@live.com", msg=f"Subject:Hello \n\n{random.choice(quotes)}")
